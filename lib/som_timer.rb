462c1de3bf607675f3541de26171eda1d701014b
Sienna Kopf <princess.kopf@gmail.com>
2020-11-02 15:39:00 -0700

    add hello_world method

--- lib/som_timer.rb
+++ lib/som_timer.rb
@@ -3,6 +3,12 @@ require "pry"
 require "json"
 
 class SomTimer
+  def self.hello_world
+    puts "Hello World! This is SomTimer, a timer that cares.
+            \nBased on the pomodoro technique™️, this application provides users with curated wellness content during break intervals.
+            \nBuild in rest, so you can focus best."
+  end
+
   def self.update_timer(work_interval, rest_interval, sound, path = "timers/1")
     service = Service.new(path)
     service.update_timer(work_interval, rest_interval, sound)
