8b2ecab6ffaee74d5f5527188fe4083e7f2ccb39
Sienna Kopf <princess.kopf@gmail.com>
2020-11-01 20:50:03 -0700

    Add one_timer and rand_exercise endpoints

--- lib/som_timer.rb
+++ lib/som_timer.rb
@@ -7,6 +7,15 @@ class SomTimer
     url = "https://som-timer-be.herokuapp.com/api/#{path}"
     response = Faraday.get(url)
     hash = JSON.parse(response.body, symbolize_names: true)
-    puts hash 
+    puts hash
+  end
+
+  def self.one_timer
+    self.perform_request("timers/1")
+  end
+
+  def self.rand_exercise(duration, category)
+    url = "rand_exercise?duration=#{duration}&category=#{category}"
+    self.perform_request(url)
   end
 end
