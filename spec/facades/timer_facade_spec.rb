947a9f979500fa473e6a35f0804da512124cbd99
Sienna Kopf <princess.kopf@gmail.com>
2020-11-03 13:09:41 -0700

    add tests for update timer facade methods

--- spec/facades/timer_facade_spec.rb
+++ spec/facades/timer_facade_spec.rb
@@ -39,5 +39,43 @@ RSpec.describe SomTimer::TimerFacade do
         end
       end
     end
+
+    describe 'PUT timer (1)' do
+      before :each do
+        path = "timers/1"
+        @facade = SomTimer::TimerFacade.new(path)
+      end
+      describe '#response_update_timer' do
+        it "calls the SomTimer API to update timer (1)" do
+          response = @facade.response_update_timer("10", "7", "chordCliff")
+
+          expect(response).to be_a Hash
+          expect(response).to have_key :id
+          expect(response).to have_key :work_interval
+          expect(response).to have_key :rest_interval
+          expect(response).to have_key :sound
+          expect(response[:id]).to eq(1)
+          expect(response[:work_interval]).to eq("10")
+          expect(response[:rest_interval]).to eq("7")
+          expect(response[:sound]).to eq("chordCliff")
+        end
+      end
+
+      describe '#update_timer' do
+        it "instantiates a timer oject from the JSON response of updating timer (1)" do
+          timer = @facade.update_timer
+
+          expect(timer).to be_a SomTimer::Timer
+          expect(timer.id).to be_a Integer
+          expect(timer.id).to eq(1)
+          expect(timer.work_interval).to be_a String
+          expect(timer.work_interval).to eq("10")
+          expect(timer.rest_interval).to be_a String
+          expect(timer.rest_interval).to eq("7")
+          expect(timer.sound).to be_a String
+          expect(timer.sound).to eq("chordCliff")
+        end
+      end
+    end
   end
 end
