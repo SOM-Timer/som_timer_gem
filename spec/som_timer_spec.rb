bcac486b87d344f3eacf41c6719c74344b829578
Sienna Kopf <princess.kopf@gmail.com>
2020-11-03 14:01:06 -0700

    update som_timer tests for rests and create_rest methods

--- spec/som_timer_spec.rb
+++ spec/som_timer_spec.rb
@@ -40,27 +40,24 @@ describe SomTimer do
 
       describe ".rests" do
         it "GET request to retrieve all rests", :vcr do
-          response = SomTimer.rests
+          rests = SomTimer.rests
 
-          expect(response).to be_a Hash
-          expect(response).to have_key :count
-          expect(response).to have_key :rests
-          expect(response[:count]).to be_a Integer
-          expect(response[:count]).to eq(1)
-          expect(response[:rests]).to be_an Array
-          rest_1 = response[:rests][0]
-          expect(rest_1).to have_key :id
-          expect(rest_1).to have_key :mood_rating_1
-          expect(rest_1).to have_key :mood_rating_2
-          expect(rest_1).to have_key :content_selected
-          expect(rest_1).to have_key :focus_interval
-          expect(rest_1).to have_key :rest_interval
-          expect(rest_1[:id]).to eq(1)
-          expect(rest_1[:mood_rating_1]).to eq(3)
-          expect(rest_1[:mood_rating_2]).to eq(5)
-          expect(rest_1[:content_selected]).to eq("MOVEMENT")
-          expect(rest_1[:focus_interval]).to eq("25")
-          expect(rest_1[:rest_interval]).to eq("5")
+          expect(rests).to be_an Array
+          expect(rests.count).to eq(9)
+          rest = rests[0]
+          expect(rest).to be_a SomTimer::Rest
+          expect(rest.id).to be_a Integer
+          expect(rest.id).to eq(1)
+          expect(rest.mood_rating_1).to be_a Integer
+          expect(rest.mood_rating_1).to eq(3)
+          expect(rest.mood_rating_2).to be_a Integer
+          expect(rest.mood_rating_2).to eq(5)
+          expect(rest.content_selected).to be_a String
+          expect(rest.content_selected).to eq("MOVEMENT")
+          expect(rest.focus_interval).to be_a String
+          expect(rest.focus_interval).to eq("25")
+          expect(rest.rest_interval).to be_a String
+          expect(rest.rest_interval).to eq("5")
         end
       end
 
@@ -98,21 +95,21 @@ describe SomTimer do
 
       describe ".create_rest" do
         it "POST request to create a rest", :vcr do
-          response = SomTimer.create_rest(2, 4, "SOMATIC", "25", "5")
+          rest = SomTimer.create_rest(2, 4, "SOMATIC", "25", "5")
 
-          expect(response).to be_a Hash
-          expect(response).to have_key :id
-          expect(response).to have_key :mood_rating_1
-          expect(response).to have_key :mood_rating_2
-          expect(response).to have_key :content_selected
-          expect(response).to have_key :focus_interval
-          expect(response).to have_key :rest_interval
-          expect(response[:id]).to eq(2)
-          expect(response[:mood_rating_1]).to eq(2)
-          expect(response[:mood_rating_2]).to eq(4)
-          expect(response[:content_selected]).to eq("SOMATIC")
-          expect(response[:focus_interval]).to eq("25")
-          expect(response[:rest_interval]).to eq("5")
+          expect(rest).to be_a SomTimer::Rest
+          expect(rest.id).to be_a Integer
+          expect(rest.id).to eq(17)
+          expect(rest.mood_rating_1).to be_a Integer
+          expect(rest.mood_rating_1).to eq(3)
+          expect(rest.mood_rating_2).to be_a Integer
+          expect(rest.mood_rating_2).to eq(5)
+          expect(rest.content_selected).to be_a String
+          expect(rest.content_selected).to eq("SOMATIC")
+          expect(rest.focus_interval).to be_a String
+          expect(rest.focus_interval).to eq("25")
+          expect(rest.rest_interval).to be_a String
+          expect(rest.rest_interval).to eq("5")
         end
       end
 
