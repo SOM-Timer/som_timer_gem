0a4957583586269a46f6e6293caad90dd94d24f9
Sienna Kopf <princess.kopf@gmail.com>
2020-11-03 13:38:46 -0700

    update som timer tests for rand_exercise and exercise to return ruby objects

--- spec/som_timer_spec.rb
+++ spec/som_timer_spec.rb
@@ -21,23 +21,20 @@ describe SomTimer do
 
       describe ".exercises" do
         it "GET request to retrieve all exercises", :vcr do
-          response = SomTimer.exercises
+          exercises = SomTimer.exercises
 
-          expect(response).to be_a Hash
-          expect(response).to have_key :count
-          expect(response).to have_key :exercises
-          expect(response[:count]).to be_a Integer
-          expect(response[:count]).to eq(33)
-          expect(response[:exercises]).to be_an Array
-          exercise_1 = response[:exercises][0]
-          expect(exercise_1).to have_key :id
-          expect(exercise_1).to have_key :url
-          expect(exercise_1).to have_key :duration
-          expect(exercise_1).to have_key :category
-          expect(exercise_1[:id]).to eq(1)
-          expect(exercise_1[:url]).to eq("https://www.youtube.com/watch?v=W5wqniA4MMc&ab_channel=EssentialSomatics")
-          expect(exercise_1[:duration]).to eq("10:00")
-          expect(exercise_1[:category]).to eq("SomaticCategory.MEDITATION")
+          expect(exercises).to be_an Array
+          expect(exercises.length).to eq(33)
+          exercise = exercises[0]
+          expect(exercise).to be_a SomTimer::Exercise
+          expect(exercise.id).to be_a Integer
+          expect(exercise.id).to eq(1)
+          expect(exercise.url).to be_a String
+          expect(exercise.url).to eq("https://www.youtube.com/watch?v=W5wqniA4MMc&ab_channel=EssentialSomatics")
+          expect(exercise.duration).to be_a String
+          expect(exercise.duration).to eq("10:00")
+          expect(exercise.category).to be_a String
+          expect(exercise.category).to eq("MEDITATION")
         end
       end
 
@@ -69,17 +66,17 @@ describe SomTimer do
 
       describe ".rand_exercise" do
         it "GET request to retrieve a random exercise", :vcr do
-          response = SomTimer.rand_exercise("10:00", "SOMATIC")
+          rand_exercise = SomTimer.rand_exercise("10:00", "SOMATIC")
 
-          expect(response).to be_a Hash
-          expect(response).to have_key :id
-          expect(response).to have_key :url
-          expect(response).to have_key :duration
-          expect(response).to have_key :category
-          expect(response[:id]).to eq(32)
-          expect(response[:url]).to eq("https://soundcloud.com/ucsdmindfulness/10-min-body-scan-by-christy-cassisa?in=ucsdmindfulness/sets/short-meditation-sessions")
-          expect(response[:duration]).to eq("10:00")
-          expect(response[:category]).to eq("SomaticCategory.SOMATIC")
+          expect(rand_exercise).to be_a SomTimer::Exercise
+          expect(rand_exercise.id).to be_a Integer
+          expect(rand_exercise.id).to eq(33)
+          expect(rand_exercise.url).to be_a String
+          expect(rand_exercise.url).to eq("https://www.youtube.com/watch?v=zsCVqFr6j1g&feature=emb_logo")
+          expect(rand_exercise.duration).to be_a String
+          expect(rand_exercise.duration).to eq("10:00")
+          expect(rand_exercise.category).to be_a String
+          expect(rand_exercise.category).to eq("SOMATIC")
         end
       end
 
