bfddb6d5f6906a8c6a80ddb31f6b644d06d5aa7d
Sienna Kopf <princess.kopf@gmail.com>
2020-11-02 22:34:55 -0700

    add test for rests endpoints (get all and post)

--- spec/som_timer_spec.rb
+++ spec/som_timer_spec.rb
@@ -41,6 +41,32 @@ describe SomTimer do
         end
       end
 
+      describe ".rests" do
+        it "GET request to retrieve all rests", :vcr do
+          response = SomTimer.rests
+
+          expect(response).to be_a Hash
+          expect(response).to have_key :count
+          expect(response).to have_key :rests
+          expect(response[:count]).to be_a Integer
+          expect(response[:count]).to eq(1)
+          expect(response[:rests]).to be_an Array
+          rest_1 = response[:rests][0]
+          expect(rest_1).to have_key :id
+          expect(rest_1).to have_key :mood_rating_1
+          expect(rest_1).to have_key :mood_rating_2
+          expect(rest_1).to have_key :content_selected
+          expect(rest_1).to have_key :focus_interval
+          expect(rest_1).to have_key :rest_interval
+          expect(rest_1[:id]).to eq(1)
+          expect(rest_1[:mood_rating_1]).to eq(3)
+          expect(rest_1[:mood_rating_2]).to eq(5)
+          expect(rest_1[:content_selected]).to eq("MOVEMENT")
+          expect(rest_1[:focus_interval]).to eq("25")
+          expect(rest_1[:rest_interval]).to eq("5")
+        end
+      end
+
       describe ".rand_exercise" do
         it "GET request to retrieve a random exercise", :vcr do
           response = SomTimer.rand_exercise("10:00", "SOMATIC")
@@ -70,6 +96,26 @@ describe SomTimer do
           expect(response[:work_interval]).to eq("25:00")
           expect(response[:rest_interval]).to eq("5:00")
           expect(response[:sound]).to eq("chordCliff")
+        end
+      end
+
+      describe ".create_rest" do
+        it "POST request to create a rest", :vcr do
+          response = SomTimer.create_rest(2, 4, "SOMATIC", "25", "5")
+
+          expect(response).to be_a Hash
+          expect(response).to have_key :id
+          expect(response).to have_key :mood_rating_1
+          expect(response).to have_key :mood_rating_2
+          expect(response).to have_key :content_selected
+          expect(response).to have_key :focus_interval
+          expect(response).to have_key :rest_interval
+          expect(response[:id]).to eq(2)
+          expect(response[:mood_rating_1]).to eq(2)
+          expect(response[:mood_rating_2]).to eq(4)
+          expect(response[:content_selected]).to eq("SOMATIC")
+          expect(response[:focus_interval]).to eq("25")
+          expect(response[:rest_interval]).to eq("5")
         end
       end
 
