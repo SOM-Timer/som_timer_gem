e3daefe10513d1380ee418a27c02f7b8047682c7
Sienna Kopf <princess.kopf@gmail.com>
2020-11-02 22:35:06 -0700

    add api calls for rests endpoints

--- lib/som_timer/service.rb
+++ lib/som_timer/service.rb
@@ -16,7 +16,15 @@ class SomTimer::Service
   end
 
   def update_timer(work_interval, rest_interval, sound)
-    to_json_post(work_interval, rest_interval, sound)
+    to_json_put(work_interval, rest_interval, sound)
+  end
+
+  def rests
+    to_json("#{@path}")
+  end
+
+  def create_rest(mood_rating_1, mood_rating_2, content_selected, focus_interval, rest_interval)
+    to_json_post(mood_rating_1, mood_rating_2, content_selected, focus_interval, rest_interval)
   end
 
   private
@@ -30,11 +38,24 @@ class SomTimer::Service
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def to_json_post(work_interval, rest_interval, sound)
+  def to_json_put(work_interval, rest_interval, sound)
     response = conn.put do |req|
       req.url "https://som-timer-be.herokuapp.com/api/#{@path}"
       req.headers['Content-Type'] = 'application/json'
       req.body = { "work_interval": "#{work_interval}", "rest_interval": "#{rest_interval}", "sound": "#{sound}" }.to_json
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def to_json_post(mood_rating_1, mood_rating_2, content_selected, focus_interval, rest_interval)
+    response = conn.post do |req|
+      req.url "https://som-timer-be.herokuapp.com/api/#{@path}"
+      req.headers['Content-Type'] = 'application/json'
+      req.body = { "mood_rating_1": "#{mood_rating_1}",
+                   "mood_rating_2": "#{mood_rating_2}",
+                   "content_selected": "#{content_selected}",
+                   "focus_interval": "#{focus_interval}",
+                   "rest_interval": "#{rest_interval}" }.to_json
     end
     JSON.parse(response.body, symbolize_names: true)
   end
