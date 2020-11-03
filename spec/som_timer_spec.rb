090a1aade45bcfbda7d4eccbf5d8b6da14c0ee74
Sienna Kopf <princess.kopf@gmail.com>
2020-11-03 13:05:51 -0700

    update som timer test based on new VCR tape

--- spec/som_timer_spec.rb
+++ spec/som_timer_spec.rb
@@ -11,11 +11,11 @@ describe SomTimer do
           expect(timer.id).to be_a Integer
           expect(timer.id).to eq(1)
           expect(timer.work_interval).to be_a String
-          expect(timer.work_interval).to eq("0.03")
+          expect(timer.work_interval).to eq("25")
           expect(timer.rest_interval).to be_a String
           expect(timer.rest_interval).to eq("5")
           expect(timer.sound).to be_a String
-          expect(timer.sound).to eq("birdChord")
+          expect(timer.sound).to eq("reverbSplash")
         end
       end
 
